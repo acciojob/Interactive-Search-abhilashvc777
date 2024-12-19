04cd850af25efa20e86081e54bc540293221c2f3
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-19 14:00:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code he
+const btn = document.querySelector(".btn")
+const div = document.querySelector(".search")
+const input = document.querySelector(".input")
+btn.addEventListener("click", ()=>{
+	div.style.transform="scale(1.2)"
+	input.focus()
+}) 
