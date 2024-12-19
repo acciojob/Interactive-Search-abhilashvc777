8f2c7befc731a0fde3b093403b24871a627b825f
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-19 14:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code he
 const btn = document.querySelector(".btn")
-const div = document.querySelector(".search")
+const div = document.querySelector(".search") 
 const input = document.querySelector(".input")
 btn.addEventListener("click", ()=>{
+	div.classList.add("active");
 	div.style.transform="scale(1.2)"
-	input.focus()
-}) 
+	input.focus() 
+})  
